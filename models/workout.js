8ddd8c908d2508c72896eb05e06fdb0f57199597
J Diaz <j.e.diaz@icloud.com>
2021-08-08 19:32:18 -0700

    had to fix schema to match seed data structure

--- models/workout.js
+++ models/workout.js
@@ -3,33 +3,41 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  type: {
-    type: String,
-    required: "Enter type of workout",
+  day: {
+    type: Date,
+    default: () => new Date(),
   },
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter name of workout",
-  },
-  duration: {
-    type: Number,
-    required: "Enter the duration",
-  },
-  wight: {
-    type: Number,
-  },
-  reps: {
-    type: Number,
-  },
-  sets: {
-    type: Number,
-  },
-  distance: {
-    type: Number,
-  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: "Enter type of workout",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter name of workout",
+      },
+      duration: {
+        type: Number,
+        required: "Enter the duration",
+      },
+      wight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
 });
 
-const Workout = mongoose.model("workout", transactionSchema);
+const Workout = mongoose.model("workout", workoutSchema);
 
 module.exports = Workout;
